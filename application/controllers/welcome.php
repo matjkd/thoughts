04a4c18c6098a0df2d69e84f2133c3d0e14b5bcd
Mat Sadler <mat@redstudio.co.uk>
2013-03-06 17:05:06 +0000

    testing getting ids

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -1,46 +1,52 @@
 <?php
-if (!defined('BASEPATH'))
-	exit('No direct script access allowed');
+	if (!defined('BASEPATH'))
+		exit('No direct script access allowed');
 
-class Welcome extends CI_Controller
-{
-
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
+	class Welcome extends CI_Controller
 	{
-		$visited = $this -> session -> userdata('visited');
 
-		if ($visited != 1)
+		/**
+		 * Index Page for this controller.
+		 *
+		 * Maps to the following URL
+		 * 		http://example.com/index.php/welcome
+		 *	- or -
+		 * 		http://example.com/index.php/welcome/index
+		 *	- or -
+		 * Since this controller is set as the default controller in
+		 * config/routes.php, it's displayed at http://example.com/
+		 *
+		 * So any other public methods not prefixed with an underscore will
+		 * map to /index.php/welcome/<method_name>
+		 * @see http://codeigniter.com/user_guide/general/urls.html
+		 */
+		public function index()
 		{
+			$this -> load -> model('thought_model');
+			$thoughts = $this -> thought_model -> get_all_thought_ids();
+			
+			print_r($thoughts);
+		
 
-			$data = array(
-			'visited' => 1,
-			'thought'=> random_string('numeric', 23)
-			);
+			$visited = $this -> session -> userdata('visited');
 
-			$this -> session -> set_userdata($data);
+			if ($visited != 1)
+			{
 
+				$data = array(
+					'visited' => 1,
+					'thought' => random_string('numeric', 3)
+				);
+
+				$this -> session -> set_userdata($data);
+
+			}
+			$number = $this -> session -> userdata('thought');
+			echo $number;
+			$this -> load -> view('welcome_message');
 		}
-$number = $this -> session -> userdata('thought');
-echo $number;
-		$this -> load -> view('welcome_message');
+
 	}
 
-}
-
-/* End of file welcome.php */
-/* Location: ./application/controllers/welcome.php */
+	/* End of file welcome.php */
+	/* Location: ./application/controllers/welcome.php */
---
+++ application/models/thought_model.php
@@ -0,0 +1,44 @@
+<?php
+
+	if (!defined('BASEPATH'))
+		exit('No direct script access allowed');
+
+	class Thought_model extends CI_Model
+	{
+
+		function __construct()
+		{
+			parent::__construct();
+		}
+
+		function get_thought($id)
+		{
+			$this -> db -> where('id', $id);
+			$query = $this -> db -> get('thoughts');
+			if ($query -> num_rows == 1)
+			{
+				return $query -> result();
+			}
+		}
+
+		function get_all_thoughts()
+		{
+			$query = $this -> db -> get('thoughts');
+			if ($query -> num_rows > 0)
+			{
+				return $query -> result();
+			}
+		}
+
+		function get_all_thought_ids()
+		{
+			$this -> db -> select('id');
+			$query = $this -> db -> get('thoughts');
+
+			if ($query -> num_rows > 0)
+			{
+				return $query -> result();
+			}
+		}
+
+	}
