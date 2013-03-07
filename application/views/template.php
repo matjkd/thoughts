c7a98c20eab9c7397fe16abf24d0b21ab6969bef
Mat Sadler <mat@redstudio.co.uk>
2013-03-07 00:27:58 +0000

    changed title

--- application/views/template.php
+++ application/views/template.php
@@ -4,7 +4,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Carousel Template &middot; Bootstrap</title>
+    <title>What I think of you</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
